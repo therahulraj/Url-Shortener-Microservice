59c1e3e48223b48b45bcd1b62dbae59202000754
rahulraj16mar <31818361+rahulraj16mar@users.noreply.github.com>
2018-08-15 11:47:23 +0530

    initial commit

---
+++ public/js/basic.js
@@ -0,0 +1,20 @@
+// $(function() {
+//
+//   $('.post').on("click", function() {
+//     var content = $('#url').val();
+//     $.ajax({
+//       type: "POST",
+//       url: `/api/shorturl/new`,
+//       dataType: "json",
+//       data: {
+//         url: content
+//       },
+//       success: function(data) {
+//
+//       },
+//       error: function() {
+//         console.log("there is error in receiving the messgae");
+//       }
+//     })
+//   })
+// })
---
+++ server.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const path = require('path');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const dns = require('dns');
+
+
+var port = process.env.PORT || 3000;
+
+var app = express();
+app.use(cors());
+app.use(bodyParser.json());
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('view engine', 'hbs');
+
+
+
+app.get('/', (req, res) => {
+res.status(200).sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+
+
+  app.post('/api/shorturl/new', urlencodedParser, (req, res) => {
+    console.log(req.body.url);
+    var url = req.body.url;
+    url = url.replace(/(^\w+:|^)\/\//, '');
+    console.log(url);
+    dns.lookup(url, (err, address, family) => {
+    if(err) {
+      return res.send({"error":"invalid URL"})
+    }
+    else {
+      return res.send({"original_url": req.body.url,"short_url":1});
+    }
+  })
+  })
+  // app.post("/api/shorturl/new",urlencodedParser, function(req, res) {
+  //   console.log(req.body);
+  //   res.send(req.body);
+  // });
+
+
+app.listen(port, () => {
+  console.log(`the server is up on ${port}`);
+})
